107acd2be73c1b009447845ad8041db3ed3e9b9f
niechao <1718659248@qq.com>
2021-11-28 16:05:06 +0800

    引入vant组件

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
   <div id="app">
     <router-view/>
     <h1>黑马头条</h1>
+    <van-button type="primary">主要按钮</van-button>
+    <div>
+      <i class="iconfont icon-fenxiang" ></i>
+    </div>
   </div>
 </template>
 <script>
